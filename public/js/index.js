808a0cb7416bb5983ff9bbe48456867aca1d8371
IKunDefined <1254729251@qq.com>
2018-08-25 17:49:03 +0800

    add annotation|merge code|add welcome-box(html)

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,62 @@
 $(function(){
     var $loginBox = $("#login-box");
     var $signupBox = $("#signup-box");
+    var $welcomeBox = $("#welcome-box");
 
     //切换面板
     $loginBox.find("a").on("click", function(){
-        $signupBox.show();
-        $loginBox.hide();
+        $signupBox.show();                                         // 显示注册框
+        $loginBox.hide();                                          // 隐藏登录框
+        
     });
     $signupBox.find("a").on("click", function(){
-        $loginBox.show();
-        $signupBox.hide();
+        $loginBox.show();                                          // 显示登录框
+        $signupBox.hide();                                         // 隐藏注册框
     });
 
-    // 通过Ajax提交请求
+    // Ajax注册模块
     $signupBox.find("input[type=button]").on("click",function(){
         $.ajax({
-            type: "post",
-            url: "/api/user/register",
-            data: {
+            type: "post",                                          // 使用方法
+            url: "/api/user/register",                             // 传入文件
+            data: {                                                // 传入数据
                 username: $signupBox.find("[name='username']").val(),
                 password: $signupBox.find("[name='password']").val(),
                 repassword: $signupBox.find("[name='repassword']").val()
             },
-            dataType: "json",
-            success: function(result) {
-                $signupBox.find(".warning").html(result.message);
-                if(!result.code){
+            dataType: "json",                                      // 数据类型
+            success: function(result) {                            // 返回数据
+                $signupBox.find(".warning").html(result.message);  // 将返回数据提示信息写入信息框
+                if(!result.code){                                  // 判断注册是否成功
                     setTimeout(function(){
-                        $loginBox.show();
-                        $signupBox.hide();
-                    }, 1000);
+                        $loginBox.show();                          // 显示登录框
+                        $signupBox.hide();                         // 隐藏注册框
+                    }, 1000);                                      // 1秒后执行
                 }
             }
         });
-    })
-})
+    });
+
+    // Ajax登录模块
+    $loginBox.find("input[type=button]").on("click",function(){
+        $.ajax({
+            type: "post",
+            url: "/api/user/login",
+            data: {
+                username: $loginBox.find("input[name='username']"),
+                password: $loginBox.find("input[name='password']")
+            },
+            dataType: "json",
+            success: function(result){
+                if(!result){
+                    $loginBox.find(".warning").html(result.message);
+                }else{
+                    setTimeout(function(){
+                        $loginBox.hide();
+                        $welcomeBox.show();
+                    },1000);
+                }
+            }
+        });
+    });
+});
--- routers/api.js
+++ routers/api.js
@@ -2,57 +2,62 @@ var express = require("express");
 var router = express.Router();
 var User = require("../modules/Users");
 
-var responseData;
+var responseData;                                   // 声明响应报文
 
 router.use(function(req, res, next){
     responseData = {
-        code: 0,
-        message: ""
+        code: 0,                                    // 初始化响应码
+        message: ""                                 // 初始化响应信息
     }
     next();  // next是什么作用？
 });
 
 router.post("/user/register", function(req, res, next){
+
     // 注册逻辑
-    var username = req.body.username;
-    var password = req.body.password;
-    var repassword = req.body.repassword;
-    if(username == "") {
-        responseData.code = 1;
-        responseData.message = "用户名不准为空！";
-        res.json(responseData);
-        return;
+    
+    var username = req.body.username;                // 获取注册用户名
+    var password = req.body.password;                // 获取注册密码
+    var repassword = req.body.repassword;            // 获取密码验证
+    
+    if(username == "") {                             // 验证用户名是否为空
+        responseData.code = 1;                       // 定义响应码
+        responseData.message = "用户名不准为空！";    // 定义响应信息
+        res.json(responseData);                      // 以JSON形式响应报文内容
+        return;                                      // 返回响应报文
     }
-    if(password == "") {
-        responseData.code = 2;
-        responseData.message = "密码不准为空！";
-        res.json(responseData);
-        return;
+
+    if(password == "") {                             // 验证密码是否为空
+        responseData.code = 2;                       // 定义响应码
+        responseData.message = "密码不准为空！";      // 定义响应信息
+        res.json(responseData);                      // 以JSON形式响应报文内容
+        return;                                      // 返回响应报文
     }
-    if(password != repassword) {
-        responseData.code = 3;
-        responseData.message = "两次输入密码不同！";
-        res.json(responseData);
-        return;
+
+    if(password != repassword) {                     // 验证确认密码是否输入相同
+        responseData.code = 3;                       // 定义响应码
+        responseData.message = "两次输入密码不同！";   // 定义响应信息
+        res.json(responseData);                      // 以JSON形式响应报文内容
+        return;                                      // 返回响应报文
     }
-    User.findOne({
-        username: username
-    }).then(function(userInfo){
-        if(userInfo){
-            responseData.code = 4;
-            responseData.message = "用户名已被注册！";
-            res.json(responseData);
-            return;
+    
+    User.findOne({                                   // 查找数据库用户名是否存在该用户名
+        username: username                         
+    }).then(function(userInfo){                      // 验证后把结果返回
+        if(userInfo){                                // 判断是否存在相同用户名
+            responseData.code = 4;                   // 定义响应码
+            responseData.message = "用户名已被注册！"; // 定义响应信息
+            res.json(responseData);                  // 以JSON形式响应报文内容
+            return;                                  // 返回响应报文
         }
-        var user = new User({
-            username: username,
-            password: password
+        var user = new User({                        // 若没有则以用户模型新建用户对象
+            username: username,                      // 用户名为注册用户名
+            password: password                       // 用户密码为注册用户密码
         });
-        return user.save();
-    }).then(function(newUserInfo){
-        console.log(newUserInfo);
-        responseData.message = "注册成功！";
-        res.json(responseData);
+        return user.save();                          // 返回该用户并调用方法将改用户存储至数据库
+    }).then(function(){                              
+        responseData.message = "注册成功！";          // 定义响应信息 
+        res.json(responseData);                      // 返回响应报文
     });
 });
 
